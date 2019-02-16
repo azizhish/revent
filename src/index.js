9eea2b48d508af4a3355934409c9140adc9522d4
shamwise <azizhish@gmail.com>
2019-02-16 18:19:45 +0400

    End of Section 7

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+
 import './index.css';
 import App from './app/layout/App';
 import "semantic-ui-css/semantic.min.css";
 import * as serviceWorker from './serviceWorker';
 
+//Redux stuff
+import { Provider } from 'react-redux'
+import { configureStore } from '../src/app/store/configureStore'
+const store = configureStore();
+
 const rootEl = document.getElementById('root');
 let render = () => {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    rootEl);
-}
-
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+    ,
+        rootEl);
+    }
+    
 if (module.hot) {
   module.hot.accept('./app/layout/App', () => { 
     setTimeout(render)
